0e243c57475e945c7d552ae69aebd86ef2312b8e
AyuWulandari25 <ayuwul1225@gmail.com>
2021-07-13 12:38:48 +0700

    commit

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ class App {
   }
 
   protected plugin(): void {
-    this.app.use(cors({ origin: "http://localhost:4200/" }));
+    this.app.use(cors({ origin: "https://new-db-agrofun.herokuapp.com" }));
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
     MongoDB();
