d18b252f1d71898a2af21d1a1fe9271b5c381924
AyuWulandari25 <ayuwul1225@gmail.com>
2021-07-21 12:42:29 +0700

    commit

--- build/models/Cart.js
+++ build/models/Cart.js
@@ -22,7 +22,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importStar(require("mongoose"));
 const cartSchema = new mongoose_1.Schema({
     productid: { type: mongoose_1.default.Types.ObjectId, ref: "Product" },
-    items: [{ type: String, ref: "Product" }],
+    items: { type: [String], ref: "Product" },
     quantity: { type: Number },
     subtotal_payment: { type: Number },
     users: { type: mongoose_1.default.Types.ObjectId, ref: "User" },
--- src/models/Cart.ts
+++ src/models/Cart.ts
@@ -4,7 +4,7 @@ import ICart from "../interfaces/cart";
 const cartSchema: Schema = new Schema(
   {
     productid: { type: mongoose.Types.ObjectId, ref: "Product" },
-    items: [{ type: String, ref: "Product" }],
+    items: { type: [String], ref: "Product" },
     quantity: { type: Number },
     subtotal_payment: { type: Number },
     users: { type: mongoose.Types.ObjectId, ref: "User" },
